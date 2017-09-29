7a3e87381d409f621404f2f6b9e366c6e7d21c9e
kmiloramirez <kmiloperlaza1996@gmail.com>
2017-09-29 13:15:54 -0500

    commit

---
+++ src/main/java/com/parqueadero/vigilante/VigilanteApplication.java
@@ -0,0 +1,12 @@
+package com.parqueadero.vigilante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilanteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/parqueadero/vigilante/VigilanteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.parqueadero.vigilante;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VigilanteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
