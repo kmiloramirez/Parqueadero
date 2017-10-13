5f160fe49f5dd5703d0b0adf917e757e92f0120f
kmiloramirez <kmiloperlaza1996@gmail.com>
2017-10-13 15:10:15 -0500

    front
    este si fue

--- src/main/java/com/parqueadero/vigilante/VigilanteConfig.java
+++ src/main/java/com/parqueadero/vigilante/VigilanteConfig.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import dominio.Parqueadero;
 import dominio.Vigilante;
@@ -44,5 +47,22 @@ public class VigilanteConfig {
 		reglasCobro.add(new ReglasCobroMoto());
 		return reglasCobro;
 	}
+	
+	 @Bean
+	    public CorsFilter corsFilter() {
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.addAllowedOrigin("*");
+	        config.addAllowedHeader("*");
+	        config.addAllowedMethod("OPTIONS");
+	        config.addAllowedMethod("GET");
+	        config.addAllowedMethod("POST");
+	        config.addAllowedMethod("PUT");
+	        config.addAllowedMethod("DELETE");
+	        source.registerCorsConfiguration("/**", config);
+	        return new CorsFilter(source);
+	    }
+	
 
 }
